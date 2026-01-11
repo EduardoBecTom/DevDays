543469b1c97897c09d5e9623c95418c54b36b284
EduardoBecTom <edubectom@alum.us.es>
2026-01-11 18:31:51 +0100

    actualizacion

--- src/middlewares/user.middleware.js
+++ src/middlewares/user.middleware.js
@@ -1,17 +1,15 @@
 import { body, validationResult } from 'express-validator';
 
 export const validateCreateUser = [
-    // Validate 'name' field
     body('name')
         .exists({ checkNull: true })
         .withMessage('Name is required')
         .isString()
         .withMessage('Name must be a string')
         .isLength({ min: 3 })
-        .withMessage('Name must be at least 3 characters long'),
-        //TODO: Añadir máximo de 50 caracteres.
-
-    // Middleware to handle validation errors
+        .withMessage('Name must be at least 3 characters long')
+        .isLength({ max: 50 })
+        .withMessage('Name must be at most 50 characters long'),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -21,6 +19,5 @@ export const validateCreateUser = [
     },
 ];
 
-import { validateCreateUser } from "../middlewares/user.middleware.js";
 
-userRouter.post('/users', validateCreateUser, addUser);
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { addUser, getUser, getUsers, removeUser, modifyUser } from "../controllers/user.controller.js";
+import { validateCreateUser } from "../middlewares/user.middleware.js";
 
 
 const userRouter = Router();
 
 userRouter.get('/users', getUsers);
 userRouter.get('/users/:id', getUser);
-userRouter.post('/users', addUser);
+userRouter.post('/users', validateCreateUser, addUser);
 userRouter.delete('/users/:id', removeUser);
 // E0 
 userRouter.put('/users/:id', modifyUser);
